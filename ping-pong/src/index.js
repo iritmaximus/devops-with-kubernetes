16b4ca9894cd843aba02e9a0ec42646f72c6d928
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2025-09-13 00:49:33 +0300

    implemented persistent storage

--- ping-pong/src/index.js
+++ ping-pong/src/index.js
@@ -1,3 +1,4 @@
+import fs from "node:fs/promise"
 import express from "express"
 
 const app = express();
@@ -5,16 +6,36 @@ const app = express();
 const PORT = process.env.PORT || 4004;
 let ppCounter = 0;
 
+const updatePPCounter = async () => {
+  let data = 0;
+  try {
+    data = await fs.readFile("/usr/src/app/files/ppcounter.txt", { encoding: "utf8" });
+    data = parseInt(data);
+  } catch (err) {
+    console.error("ERROR:", err);
+    return -1;
+  }
+
+  console.log("PPCounter:", data);
+
+  await fs.writeFile("/usr/src/app/files/datehash.txt", ++data, err => {
+    if (err) console.error("ERROR:", err);
+  });
+
+  return data
+}
+
 app.get("/", (req, res) => {
-    return res.send({ "message": "Try /pingpong :)"});
+  return res.send({ "message": "Try /pingpong :)"});
 });
 
-app.get("/pingpong", (req, res) => {
-    return res.send({ "pong": ppCounter++ });
+app.get("/pingpong", async (req, res) => {
+  const ppCounter = await updatePPCounter();
+  return res.send({ "pong": ppCounter });
 });
 
 const server = app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 
