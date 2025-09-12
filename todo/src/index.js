52e065b478b2990fb370a85c7a3fd28c9dd4893a
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2025-09-12 11:46:28 +0300

    changed default ports to different ports

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
 app.get("/ping", (req, res) => {
     return res.send({ "message": "pong" });
